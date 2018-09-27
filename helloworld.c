1b2c7e6564028604f1fb553b0bf21a233110399e
ABHI-MX <43606886+ABHI-MX@users.noreply.github.com>
2018-09-27 18:06:40 +0530

    Update helloworld.c

--- helloworld.c
+++ helloworld.c
@@ -1,1 +1,11 @@
 
+#include <stdio.h> //golbal decration
+
+int main()  // local declaration
+  
+{
+  
+   printf("Hello, World!"); // printf() displays the charters inside quotation
+ 
+   return 0;
+}
